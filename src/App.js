dc3bdbfbe20ba7638787f686a8507d0782e0ca30
Lee Borsboom <lborsboom@console.com.au>
2020-08-24 13:16:32 +1000

    Reproduction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { gql, useQuery, useMutation } from "@apollo/client";
 
-const ALL_PEOPLE = gql`
-  query AllPeople {
-    people {
+const PEOPLE_AND_STUFF = gql`
+  query AllPeople($textSearch: String) {
+    stuff {
+      something
+    }
+    people(textSearch: $textSearch) {
       id
       name
     }
@@ -20,27 +23,27 @@ const ADD_PERSON = gql`
 `;
 
 export default function App() {
-  const [name, setName] = useState('');
-  const {
-    loading,
-    data,
-  } = useQuery(ALL_PEOPLE);
+  const [textSearch, setTextSearch] = useState("");
+  const [name, setName] = useState("");
+  const { loading, data, networkStatus } = useQuery(PEOPLE_AND_STUFF, {
+    notifyOnNetworkStatusChange: true,
+    variables: textSearch === "" ? {} : { textSearch }
+  });
+
+  console.log(JSON.stringify({ loading, networkStatus, data }, null, 2))
 
   const [addPerson] = useMutation(ADD_PERSON, {
     update: (cache, { data: { addPerson: addPersonData } }) => {
-      const peopleResult = cache.readQuery({ query: ALL_PEOPLE });
+      const peopleResult = cache.readQuery({ query: PEOPLE_AND_STUFF });
 
       cache.writeQuery({
-        query: ALL_PEOPLE,
+        query: PEOPLE_AND_STUFF,
         data: {
           ...peopleResult,
-          people: [
-            ...peopleResult.people,
-            addPersonData,
-          ],
-        },
+          people: [...peopleResult.people, addPersonData]
+        }
       });
-    },
+    }
   });
 
   return (
@@ -51,27 +54,33 @@ export default function App() {
       </p>
       <div className="add-person">
         <label htmlFor="name">Name</label>
-        <input 
-          type="text" 
-          name="name" 
+        <input
+          type="text"
+          name="name"
           value={name}
-          onChange={evt => setName(evt.target.value)}
+          onChange={(evt) => setName(evt.target.value)}
         />
         <button
           onClick={() => {
             addPerson({ variables: { name } });
-            setName('');
+            setName("");
           }}
         >
           Add person
         </button>
       </div>
       <h2>Names</h2>
+      <input
+        type="text"
+        value={textSearch}
+        placeholder="search text"
+        onChange={(event) => setTextSearch(event.target.value)}
+      />
       {loading ? (
         <p>Loading…</p>
       ) : (
         <ul>
-          {data?.people.map(person => (
+          {data?.people.map((person) => (
             <li key={person.id}>{person.name}</li>
           ))}
         </ul>
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -3,52 +3,75 @@ import {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
-  GraphQLList,
-} from 'graphql';
+  GraphQLList
+} from "graphql";
 
 const PersonType = new GraphQLObjectType({
-  name: 'Person',
+  name: "Person",
   fields: {
     id: { type: GraphQLID },
-    name: { type: GraphQLString },
-  },
+    name: { type: GraphQLString }
+  }
 });
 
 const peopleData = [
-  { id: 1, name: 'John Smith' },
-  { id: 2, name: 'Sara Smith' },
-  { id: 3, name: 'Budd Deey' },
+  { id: 1, name: "John Smith" },
+  { id: 2, name: "Sara Smith" },
+  { id: 3, name: "Budd Deey" }
 ];
 
 const QueryType = new GraphQLObjectType({
-  name: 'Query',
+  name: "Query",
   fields: {
     people: {
+      args: {
+        textSearch: { type: GraphQLString }
+      },
       type: new GraphQLList(PersonType),
-      resolve: () => peopleData,
+      resolve: (parent, args, context) => {
+        console.log('Resolving query', JSON.stringify({ args }, null, 2))
+        if (args.textSearch) {
+          return peopleData.filter((person) =>
+            person.name.toUpperCase().includes(args.textSearch.toUpperCase())
+          );
+        }
+        return peopleData;
+      }
     },
-  },
+    stuff: {
+      type: new GraphQLObjectType({
+        name: 'Stuff',
+        fields: {
+          something: { type: GraphQLString }
+        }
+      }),
+      resolve: () => ({ something: 'blah' })
+    }
+  }
 });
 
 const MutationType = new GraphQLObjectType({
-  name: 'Mutation',
+  name: "Mutation",
   fields: {
     addPerson: {
       type: PersonType,
-      args: { 
-        name: { type: GraphQLString },
+      args: {
+        name: { type: GraphQLString }
       },
       resolve: function (_, { name }) {
         const person = {
           id: peopleData[peopleData.length - 1].id + 1,
-          name,
+          name
         };
 
         peopleData.push(person);
         return person;
       }
-    },
-  },
+    }
+  }
 });
 
-export const schema = new GraphQLSchema({ query: QueryType, mutation: MutationType });
+export const schema = new GraphQLSchema({
+  query: QueryType,
+  mutation: MutationType
+});
